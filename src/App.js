f52d01a38546cd0f2d2ab0d78b015addf64ca61c
Aesop Wolf <aesopwolf@gmail.com>
2016-01-04 23:34:03 -0800

    show the same rendered slide that the user is currently editing

--- src/App.js
+++ src/App.js
@@ -16,13 +16,38 @@ export class App extends Component {
     return {__html: html};
   }
 
+  // show the same rendered slide that the user is currently editing
+  handleCursorPosition(event) {
+    var self = this;
+    var cursor = event.target.selectionStart;
+
+    // split on hr's
+    var regexp = /(?:\n){2,}^( *[-*_]){3,}(?:\n){2,}/gm;
+    var pageBreak = event.target.value.match(regexp);
+
+    if(pageBreak) {
+      var pageBreakPositions = [0]
+      pageBreak.forEach(function(value, index, array) {
+        var pageBreakPosition = self.state.input.indexOf(value) + 2;
+        pageBreakPositions.push(pageBreakPosition);
+      });
+
+      pageBreakPositions.forEach(function(value, index, array) {
+        if(cursor >= value) {
+          self.setState({
+            slide: index
+          })
+        }
+      });
+    }
+  }
+
   handleMarkdown(event) {
     var markdown = marked(event.target.value)
     var slides = markdown.split("<hr>");
     this.setState({
       input: event.target.value,
-      slides: slides,
-      slide: slides.length - 1
+      slides: slides
     })
   }
 
@@ -33,10 +58,10 @@ export class App extends Component {
   }
 
   render() {
-    var frameSrc = 'data:text/html;charset=UTF-8,<html><body>' + (this.state.slides[this.state.slide] || '') + '</body></html>';
+    var frameSrc = 'data:text/html;charset=UTF-8,<html><body class="slide-' + this.state.slide + '">' + (this.state.slides[this.state.slide] || '') + '</body></html>';
     return (
       <div>
-        <textarea onChange={this.handleMarkdown.bind(this)} value={this.state.input}/>
+        <textarea onChange={this.handleMarkdown.bind(this)} value={this.state.input} onClick={this.handleCursorPosition.bind(this)} onKeyUp={this.handleCursorPosition.bind(this)}/>
         <iframe ref="myIframe" src={frameSrc}></iframe>
         <input type="range" name="points" min="0" max={this.state.slides.length - 1} onChange={this.changeSlide.bind(this)} value={this.state.slide}/>
       </div>
