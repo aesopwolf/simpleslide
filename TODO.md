20168b8b3229289720b258cea0a151fb572d49eb
Aesop Wolf <aesopwolf@gmail.com>
2016-01-05 14:52:49 -0800

    add fullscreen mode with keyboard shortcuts

--- TODO.md
+++ TODO.md
@@ -1,8 +1,18 @@
 TODO
 ----
 
-1.	better ux and layout
-2.	color scheme
-3.	fonts
-4.	add support for math (probably through mathjax)
-5.	add IE support by ditching data uri
+MVP:
+
+1.	hide fullscreen toggle after 3 seconds of no mouse activity
+2.	display fullscreen toggle after mouse activity
+3.	default css
+4.	custom css
+
+Next version:
+
+1.	show slide number in url
+2.	show slide number on bottom right of slide (css to hide?)
+3.	data persistence
+4.	control slides from another tab (for mobile)
+5.	add support for math (probably through mathjax)
+6.	add IE support by ditching data uri
--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import marked from 'marked'
 export class App extends Component {
   constructor(props) {
     super(props);
+    window.addEventListener('keydown', this.handleKeypress.bind(this));
     this.state = {
       markdown: '',
       slides: [''],
       slide: 0,
+      fullscreen: false,
       input: `SimpleSlide
 -----------
 
@@ -26,6 +28,29 @@ If you have any questions you can email me at [aesopwolf@gmail.com](mailto:aesop
 
 Or you can [file a bug report](https://github.com/aesopwolf/simpleslide/issues)
 `
+    };
+  }
+
+  handleKeypress(event) {
+    if(this.state.fullscreen) {
+      var length = this.state.slides.length - 1;
+      switch(event.keyCode) {
+        case 37: // left arrow
+          this.setState({
+            slide: this.state.slide - 1 >= 0 ? this.state.slide - 1 : 0
+          })
+          break;
+        case 39: // right arrow
+          this.setState({
+            slide: this.state.slide + 1 <= length ? this.state.slide + 1 : length
+          })
+          break;
+        case 27: // esc
+          this.setState({
+            fullscreen: false
+          })
+          break;
+      }
     }
   }
 
@@ -90,15 +115,27 @@ Or you can [file a bug report](https://github.com/aesopwolf/simpleslide/issues)
     })
   }
 
+  fullscreenToggle() {
+    this.setState({
+      fullscreen: !this.state.fullscreen
+    })
+  }
+
   render() {
     var frameSrc = 'data:text/html;charset=UTF-8,<html><body class="slide-' + this.state.slide + '">' + (this.state.slides[this.state.slide] || '') + '</body></html>';
     return (
       <div className="container-fluid fullHeight">
+        <If condition={this.state.fullscreen}>
+          <div className="fullscreen">
+            <iframe ref="myIframe" src={frameSrc} style={{width: '100%'}}></iframe>
+          </div>
+        </If>
         <div className="row fullHeight">
           <div className="col-xs-6 fullHeight">
             <textarea className="fullHeight" onChange={this.handleMarkdown.bind(this)} value={this.state.input} onClick={this.handleCursorPosition.bind(this)} onKeyUp={this.handleCursorPosition.bind(this)}/>
           </div>
           <div className="col-xs-6 fullHeight preview aligner">
+            <span onClick={this.fullscreenToggle.bind(this)} className="fullscreenToggle glyphicon glyphicon-fullscreen" />
             <div className="alignerItem">
               <iframe ref="myIframe" src={frameSrc} style={{width: '100%'}}></iframe>
               <input type="range" name="points" min="0" max={this.state.slides.length - 1} onChange={this.changeSlide.bind(this)} value={this.state.slide}/>
