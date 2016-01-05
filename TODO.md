0f03185647e5780adab6467906ec424b5d2dc9c6
Aesop Wolf <aesopwolf@gmail.com>
2016-01-04 19:34:49 -0800

    add slider and separate slides by an <hr> element

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 TODO
 ----
 
-1. add support for math (probably through mathjax)
+1.	add support for math (probably through mathjax)
+2.	add IE support by ditching data uri
--- src/App.js
+++ src/App.js
@@ -5,26 +5,40 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: ''
+      markdown: '',
+      slides: [''],
+      slide: 0
     }
   }
 
   createMarkup() {
-    return {__html: marked(this.state.markdown)};
+    var html = marked(this.state.input)
+    return {__html: html};
   }
 
   handleMarkdown(event) {
+    var markdown = marked(event.target.value)
+    var slides = markdown.split("<hr>");
     this.setState({
-      markdown: event.target.value
+      input: event.target.value,
+      slides: slides,
+      slide: slides.length - 1
+    })
+  }
+
+  changeSlide(event) {
+    this.setState({
+      slide: event.target.value
     })
   }
 
   render() {
-    var frameSrc = 'data:text/html;charset=UTF-8,<html><body>' + marked(this.state.markdown) + '</body></html>';
+    var frameSrc = 'data:text/html;charset=UTF-8,<html><body>' + (this.state.slides[this.state.slide] || '') + '</body></html>';
     return (
       <div>
-        <textarea onChange={this.handleMarkdown.bind(this)} value={this.state.markdown}/>
+        <textarea onChange={this.handleMarkdown.bind(this)} value={this.state.input}/>
         <iframe ref="myIframe" src={frameSrc}></iframe>
+        <input type="range" name="points" min="0" max={this.state.slides.length - 1} onChange={this.changeSlide.bind(this)} value={this.state.slide}/>
       </div>
     );
   }
