f43aa86e971d16fffec86fa902377cf89de9670a
Aesop Wolf <aesopwolf@gmail.com>
2016-01-04 18:07:29 -0800

    add marked library to convert textarea markdown to html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import marked from 'marked'
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: ''
+    }
+  }
+
+  createMarkup() {
+    return {__html: marked(this.state.markdown)};
+  }
+
+  handleMarkdown(event) {
+    this.setState({
+      markdown: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        hello world
+        <textarea onChange={this.handleMarkdown.bind(this)} value={this.state.markdown}/>
+        <div dangerouslySetInnerHTML={this.createMarkup()} />
       </div>
     );
   }
